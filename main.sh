ddc040d0efdb44b3b41401730d7c5fce5b82f952
Peter Dave Hello <hsu@peterdavehello.org>
2017-03-05 05:20:47 +0800

    Message updates

--- main.sh
+++ main.sh
@@ -29,10 +29,10 @@ fi
 
 if [ "${DRONE_BUILD_STATUS}" = "success" ]; then
     REVIEW_EVENT="APPROVE"
-    REVIEW_CONTENT="@${DRONE_COMMIT_AUTHOR} congratulations! ${DRONE_REPO_LINK}/commit/${DRONE_COMMIT_SHA} CI test passed!\nPlease wait for the further review from the maintainers!\n\n For the details, please take a look at ${DRONE_BUILD_LINK}"
+    REVIEW_CONTENT="@${DRONE_COMMIT_AUTHOR} congratulations! ${DRONE_REPO_LINK}/commit/${DRONE_COMMIT_SHA} CI test passed! :white_check_mark: \nPlease wait for the further review from the maintainers!\n\n For the details :page_with_curl:, please take a look at :arrow_right: ${DRONE_BUILD_LINK}, thank you :grinning:"
 else
     REVIEW_EVENT="REQUEST_CHANGES"
-    REVIEW_CONTENT="${DRONE_REPO_LINK}/commit/${DRONE_COMMIT_SHA} CI test failed!\n\n@${DRONE_COMMIT_AUTHOR} please take a look at CI build ${DRONE_BUILD_LINK} for details!\n Most of the error will have corresponding explanation, so that you will know what's wrong and then try to fix it!\n If you cannot understand the error message and still need help, feel free to ask help from our maintainers!"
+    REVIEW_CONTENT="Oops :fearful: ${DRONE_REPO_LINK}/commit/${DRONE_COMMIT_SHA} CI test failed :exclamation: \n\n@${DRONE_COMMIT_AUTHOR} please take a look at CI build ${DRONE_BUILD_LINK} for details :memo: !\n Most of the error will have corresponding explanation, so that you will know what's wrong and then try to fix it!\n If you cannot understand the error message and need help, feel free to ask  our maintainers :relaxed: "
 fi
 
 API_URL="https://api.github.com/repos/${DRONE_REPO}/pulls/${DRONE_PULL_REQUEST}/reviews"
